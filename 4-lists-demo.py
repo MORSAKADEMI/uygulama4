7222a01b90323707589c230b7eeb934b1ae76a02
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2022-09-01 18:21:03 +0300

    Create 4-lists-demo.py

---
+++ 4-lists-demo.py
@@ -0,0 +1,45 @@
+# 1-  "Samsung S5,Samsung S6,Samsung S7,Samsung S8" elemanlarına sahip bir liste oluşturunuz.
+
+
+# 2-  Liste Kaç elemanlıdır ?
+
+
+# 3-  Listenin ilk ve son elemanı nedir ?
+
+
+# 4-  "Samsung S5" değerini "Samsung S9" ile değiştirin.
+# telefonlar[0] = "Samsung S9"
+
+
+# 5-  "Samsung S6" listenin bir elemanı mıdır ?
+
+
+# 6-  Listenin -3 indeksindeki değer nedir ?
+
+
+# 7-  Listenin ilk 2 elemanını alın.
+
+
+# 8-  Listenin son 2 elemanı yerine "Samsung S9" ve "Samsung S10" değerlerini ekleyin.
+
+
+# 9-  Listenin üzerine "IPhone X" ve "IPhone XR" değerlerini ekleyin.
+
+
+# 10- Listenin son elemanını silin.
+
+
+
+# 11- Liste elemanlarını tersten yazdırınız.
+
+
+# 12- Aşağıdaki verileri bir liste içinde saklayınız. 
+
+      # kullaniciA: Yiğit Bilgi 2010, (70,60,70)-1
+      # kullaniciB: Sena Turan  1999, (80,80,70)
+      # kullaniciC: Ahmet Turan 1998, (80,70,90) 
+
+
+
+# 13- Liste elemanlarını ekrana yazdırınız.
+
